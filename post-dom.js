2e0fd8c9d0f3edb3e6f8a27bb96cabac43436986
SpandanBG <spandanbg@gmail.com>
2024-08-02 00:34:41 +0530

    finalize base template and functionality

--- post-dom.js
+++ post-dom.js
@@ -1,22 +1,26 @@
-let notes = [
+const notes = [
   { name: "About", file: "ABOUT.md" }
 ]
+
+function loadFile(selectedIdx) {
+  const note = notes[selectedIdx];
+
+  file_name.innerHTML = note.file
+  file_content.src = `content?file=${note.file}`
+}
 
 const files = document.getElementById("files");
 const file = document.getElementById("current-file");
 const file_name = document.getElementById("file-name");
 const file_content = document.getElementById("file-content");
 
-notes.forEach(note => {
+notes.forEach(({ name }, i) => {
   const noteLink = document.createElement("span")
-  noteLink.innerHTML = note.name
+  noteLink.innerHTML = name
   noteLink.className = "cursor-pointer"
-
-  noteLink.onclick = () => {
-    file_name.innerHTML = note.file
-    file_content.src = `content?file=${note.file}`
-  }
-
+  noteLink.onclick = () => loadFile(i)
   files.append(noteLink)
 });
 
+// Load first file by default
+loadFile(0);
